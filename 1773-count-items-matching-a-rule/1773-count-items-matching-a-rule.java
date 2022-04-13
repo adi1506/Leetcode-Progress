9d84a9013fcee427d79300d0d15920b349110942
Aditya Chatterjee <48512350+adi1506@users.noreply.github.com>
2022-04-13 12:02:43 +0530

    Time: 3 ms (97.92%), Space: 47 MB (89.68%) - LeetHub

--- 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.java
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.java
@@ -1,19 +1,38 @@
 class Solution {
     public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
-        int ctr = 0;
+//         //1st attempt : brute force
+//         int ctr = 0;
         
-        for(List<String> item : items){        
-            if(ruleKey.equals("type") && ruleValue.equals(item.get(0))){
-                ctr++;
-            }
-            else if(ruleKey.equals("color") && ruleValue.equals(item.get(1))){
-                ctr++;
-            }
-            else if(ruleKey.equals("name") && ruleValue.equals(item.get(2))){
-                ctr++;
-            }
+//         //iterating over items and checking where ruleKey & ruleValue matches and updating that it in the ctr
+//         for(List<String> item : items){        
+//             if(ruleKey.equals("type") && ruleValue.equals(item.get(0))){
+//                 ctr++;
+//             }
+//             else if(ruleKey.equals("color") && ruleValue.equals(item.get(1))){
+//                 ctr++;
+//             }
+//             else if(ruleKey.equals("name") && ruleValue.equals(item.get(2))){
+//                 ctr++;
+//             }
+//         }
+        
+//         return ctr;
+        
+        //2nd attempt : finding the type first
+        
+        int ctr = 0;
+        int type = -1;
+        
+        switch(ruleKey){
+                case "type" -> type = 0;
+                case "color" -> type = 1;
+                case "name" -> type = 2;
         }
         
+        for(List<String> item : items){
+            if(item.get(type).equals(ruleValue))
+                ctr++;
+        }
         return ctr;
     }
 }
