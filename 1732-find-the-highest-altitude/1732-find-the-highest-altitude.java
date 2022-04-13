fd38eaa5e47c4715e99a58a39b456045aea08976
Aditya Chatterjee <48512350+adi1506@users.noreply.github.com>
2022-04-13 12:22:18 +0530

    Time: 0 ms (100.00%), Space: 40 MB (89.53%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
@@ -1,18 +1,36 @@
 class Solution {
     public int largestAltitude(int[] gain) {
-        //1st attempt
-        for(int i = 1; i<gain.length;i++){
-            gain[i] += gain[i-1];
+//         //1st attempt
+        
+//         //find the running sum of the array
+//         //Eg i/p => [-5,1,5,0,-7]
+//         //Eg running sum => [-5,-4,1,1,-6]
+//         for(int i = 1; i<gain.length;i++){
+//             gain[i] += gain[i-1];
+//         }
+        
+//         //initialising max value
+//         int max = Integer.MIN_VALUE;
+        
+//         //finding the max value from the running sum array
+//         for(int i : gain){
+//             max = Math.max(max,i);
+//         }
+        
+        
+//         if(max < 0) return 0;
+//         else return max;
+        
+        //2nd Attempt: less code approach
+        
+        int sum =0 ;
+        int max = 0;
+        
+        for(int i = 0;i <gain.length;i++){
+            sum+= gain[i];
+            if(sum > max) max = sum;
         }
         
-        int max = Integer.MIN_VALUE;
-        
-        for(int i : gain){
-            max = Math.max(max,i);
-        }
-        
-        if(max < 0) return 0;
-        else return max;
-        
+        return max;
     }
 }
