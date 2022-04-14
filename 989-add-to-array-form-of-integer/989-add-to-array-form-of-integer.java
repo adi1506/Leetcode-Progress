7aba3d47eda94ef20f2e65b6c547c66c248cf831
Aditya Chatterjee <48512350+adi1506@users.noreply.github.com>
2022-04-14 15:32:19 +0530

    Time: 51 ms (20.41%), Space: 44.1 MB (82.97%) - LeetHub

--- 989-add-to-array-form-of-integer/989-add-to-array-form-of-integer.java
+++ 989-add-to-array-form-of-integer/989-add-to-array-form-of-integer.java
@@ -1,26 +1,6 @@
 class Solution {
     public List<Integer> addToArrayForm(int[] num, int k) {
-        //1st attempt : brute force : all cases were not cleared
-        // eg [9,9,9,9,9,9,9,9,9,9]
-        //     1
-        
-//         long sum = 0;
-//         int len = num.length;
-//         for(int i = 0; i < len; i++){
-//             sum += num[i] * Math.pow(10,len-i-1);
-//         }
-        
-//         sum += k;
-//         int sumDig = (int)Math.floor(Math.log10(sum)+1);
-//         List<Integer> sumArr = new ArrayList<Integer>(sumDig);
-        
-//         for(int i = 0 ; i < sumDig; i++){
-//             sumArr.add(0, (int)sum%10);
-//             sum /= 10;
-//         }
 
-        
-//         return sumArr;
                   
         //2nd attempt : after seeing from youtube
         int len = num.length;
