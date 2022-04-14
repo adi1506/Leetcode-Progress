d5e1261796656d3051fae35b7b3669546b75dd42
Aditya Chatterjee <48512350+adi1506@users.noreply.github.com>
2022-04-14 08:16:20 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (90.13%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
@@ -1,15 +1,36 @@
 class Solution {
     public int diagonalSum(int[][] mat) {
-        //1st attempt : brute force
+        //1st attempt : brute force : TO O(n^2)
+        
+//         int sum = 0;
+        
+//         //iterating through every element
+//         for(int i = 0; i < mat.length; i++){
+//             for(int j = 0; j<mat[i].length;j++){
+//                 //for primary diagonal -> i==j
+//                 //for secondary diagonal -> i+j == mat[i].length -1
+//                 if(i==j || i+j == mat[i].length-1)
+//                     sum += mat[i][j];
+//             }
+//         }
+//         return sum;
+        
+        //2nd Attempt : Optimised : TO O(n)
         
         int sum = 0;
+        int n = mat.length;
         
-        for(int i = 0; i < mat.length; i++){
-            for(int j = 0; j<mat[i].length;j++){
-                if(i==j || i+j == mat[i].length-1)
-                    sum += mat[i][j];
-            }
+        //just a single loop
+        for(int i = 0; i < n; i++){
+            sum += mat[i][i]; //for primary diagonal
+            sum += mat[i][n-i-1]; //for secondary diagonal
         }
-        return sum;
+        
+        // if(n%2 == 1){
+        //     sum -= mat[(n-1)/2][(n-1)/2];
+        // }
+        
+        //for odd matrix, central item is added twice, so removing it once 
+        return n%2 == 0 ? sum : sum - mat[n/2][n/2];
     }
 }
