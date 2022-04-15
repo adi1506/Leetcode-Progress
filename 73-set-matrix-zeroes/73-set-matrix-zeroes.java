66f61f17badb32a08544da19db16ff5902d69b77
Aditya Chatterjee <48512350+adi1506@users.noreply.github.com>
2022-04-15 12:07:44 +0530

    Time: 1 ms (88.83%), Space: 43.8 MB (87.84%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -34,33 +34,49 @@ class Solution {
         //2nd attempt : taking 2 dummy 1D arrays and storing the position of 0's.
         //TC : O(M*N)
         //SC : O(M+N)
+//         int M = mat.length;
+//         int N = mat[0].length;
+//         int[] arrM = new int[M];
+//         int[] arrN = new int[N];
+            
+//         for(int i = 0; i < M; i++){
+//             for(int j = 0; j < N; j++){
+//                 if(mat[i][j] == 0){
+//                     arrM[i] = -1;
+//                     arrN[j] = -1;
+//                 }
+//             }
+//         }
+        
+//         for(int i = 0; i < M; i++){
+//             for(int j = 0; j < N; j++){
+//                 if(mat[i][j] != 0 && (arrM[i] == -1 || arrN[j] == -1)){
+//                     mat[i][j] = 0;
+//                 }
+//             }
+//         }
+        
+        //3rd attempt : most optimal approach, using no extra space
+        
         int M = mat.length;
         int N = mat[0].length;
-        int[] arrM = new int[M];
-        int[] arrN = new int[N];
-        
-        // for(int i = 0; i<M ; i++){
-        //     arrM[i] = -1;
-        // }
-        // for(int i = 0; i<N ; i++){
-        //     arrN[i] = -1;
-        // }
+        int col = 1;
         
         for(int i = 0; i < M; i++){
-            for(int j = 0; j < N; j++){
-                if(mat[i][j] == 0){
-                    arrM[i] = -1;
-                    arrN[j] = -1;
-                }
+            if(mat[i][0] == 0) col = 0;
+            for(int j = 1; j < N; j++){
+                if(mat[i][j] == 0)
+                    mat[i][0] = mat[0][j] = 0;
             }
         }
         
-        for(int i = 0; i < M; i++){
-            for(int j = 0; j < N; j++){
-                if(mat[i][j] != 0 && (arrM[i] == -1 || arrN[j] == -1)){
+        for(int i = M-1; i>=0; i--){
+            for(int j = N-1; j>=1; j--){
+                if(mat[i][0] == 0 || mat[0][j] == 0)
                     mat[i][j] = 0;
-                }
             }
+            if(col == 0)
+                mat[i][0] = 0;
         }
         
         
