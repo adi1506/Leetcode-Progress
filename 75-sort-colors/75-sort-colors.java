272a8ec986adf2c13a20b199c644e8cffdf5cadd
Aditya Chatterjee <48512350+adi1506@users.noreply.github.com>
2022-04-16 13:48:42 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (93.00%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,34 +1,64 @@
 class Solution {
     public void sortColors(int[] nums) {
+        //can also be done using any sorting algo 
+            //for that TC O(nlogn)
         
-        //1st attempt : brute force : TC O(n) SC O(1)
-        int r = 0;
-        int w = 0;
-        int b = 0;
         
-        //counting each number of colors
-        for(int i = 0; i<nums.length; i++){
-            if(nums[i] == 0)
-                r++;
-            else if(nums[i] == 1)
-                w++;
-            else if(nums[i] == 2)
-                b++;
+//         //1st attempt : brute force : TC O(2*n) SC O(1)
+//         int r = 0;
+//         int w = 0;
+//         int b = 0;
+        
+//         //counting each number of colors
+//         for(int i = 0; i<nums.length; i++){
+//             if(nums[i] == 0)
+//                 r++;
+//             else if(nums[i] == 1)
+//                 w++;
+//             else if(nums[i] == 2)
+//                 b++;
+//         }
+        
+//         //adding 0 (red color) in r times
+//         for(int i = 0; i<r; i++){
+//             nums[i] = 0;
+//         }
+        
+//         //adding white color w times starting from rth postion to (r+w)th position
+//         for(int i = r; i<w+r; i++){
+//             nums[i] = 1;
+//         }
+        
+//         //adding blue color b times starting from (r+w)th postion to (r+w+b)th position
+//         for(int i = r+w; i<r+w+b; i++){
+//             nums[i] = 2;
+//         }
+        
+        //2nd attempt : Dutch National Flag Algorithm
+        
+        int l = 0;
+        int m = 0;
+        int h = nums.length - 1;
+        
+        while(m <= h){
+            if(nums[m] == 0){
+                swap(nums,l,m);
+                l++;
+                m++;
+            }
+            else if(nums[m] == 1){
+                m++;
+            }
+            else if(nums[m] == 2){
+                swap(nums,m,h);
+                h--;
+            }
         }
-        
-        //adding 0 (red color) in r times
-        for(int i = 0; i<r; i++){
-            nums[i] = 0;
-        }
-        
-        //adding white color w times starting from rth postion to (r+w)th position
-        for(int i = r; i<w+r; i++){
-            nums[i] = 1;
-        }
-        
-        //adding blue color b times starting from (r+w)th postion to (r+w+b)th position
-        for(int i = r+w; i<r+w+b; i++){
-            nums[i] = 2;
-        }
+            
+    }
+    public void swap(int[] arr,int i,int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
