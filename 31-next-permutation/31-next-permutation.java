2681fd7cf2040f382850bb6858dc301bcf06000d
Aditya Chatterjee <48512350+adi1506@users.noreply.github.com>
2022-04-16 09:02:00 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (95.32%) - LeetHub

--- 31-next-permutation/31-next-permutation.java
+++ 31-next-permutation/31-next-permutation.java
@@ -1,26 +1,67 @@
+//1st attempt
+
+// class Solution {
+//     public void nextPermutation(int[] A) {
+        
+//         if(A == null || A.length <= 1) return;
+//         int i = A.length - 2;
+//         while(i >= 0 && A[i] >= A[i + 1]) i--; 
+//         if(i >= 0) {                           
+//             int j = A.length - 1;              
+//             while(A[j] <= A[i]) j--;      
+//             swap(A, i, j);                     
+//         }
+//         reverse(A, i + 1, A.length - 1);    
+//     }
+        
+//     public void swap(int[] A, int i, int j) {
+//         int tmp = A[i];
+//         A[i] = A[j];
+//         A[j] = tmp;
+//     }
+
+//     public void reverse(int[] A, int i, int j) {
+//         while(i < j) swap(A, i++, j--);
+//     }
+// }
+
+
 class Solution {
-    public void nextPermutation(int[] A) {
+    public void nextPermutation(int[] nums) {
+        //edge case
+        if(nums.length <= 1 || nums == null ) return;
         
-        if(A == null || A.length <= 1) return;
-        int i = A.length - 2;
-        while(i >= 0 && A[i] >= A[i + 1]) i--; 
-        if(i >= 0) {                           
-            int j = A.length - 1;              
-            while(A[j] <= A[i]) j--;      
-            swap(A, i, j);                     
+        //nums[i] <= nums[i+1];
+        
+        int i = nums.length - 2;
+        while(i>=0 && nums[i] >= nums[i+1] ) i--;
+        
+        //if i<0 then that means the number does not have a larger lexicographical rearrangement
+        // therefore it will become the 1st possible rearrangement
+        if(i>=0){
+            //nums[i] <= nums[j]
+            int j = nums.length - 1;
+            while(nums[j] <= nums[i]) j--;
+            
+            //swap i and j
+            swap(nums, i, j);
         }
-        reverse(A, i + 1, A.length - 1);    
+        
+        //reverse from i+1 till end
+        reverse(nums, i+1, nums.length - 1);
+        
+        
+    }
+    
+    public void swap(int[] A, int i, int j){
+        int temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
+    }
+    
+    public void reverse(int[] A, int i, int j){
+        while(i<j) swap(A,i++,j--);
     }
         
-    public void swap(int[] A, int i, int j) {
-        int tmp = A[i];
-        A[i] = A[j];
-        A[j] = tmp;
-    }
-
-    public void reverse(int[] A, int i, int j) {
-        while(i < j) swap(A, i++, j--);
-    }
+    
 }
-
-
