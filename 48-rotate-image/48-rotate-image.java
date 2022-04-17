0ee7741eb1595d0bf572e3933fa24337fea4651b
Aditya Chatterjee <48512350+adi1506@users.noreply.github.com>
2022-04-17 21:17:14 +0530

    Time: 1 ms (6.89%), Space: 42.2 MB (76.33%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -3,17 +3,15 @@ class Solution {
         int n = matrix.length;
         for(int i = 0; i < n ; i++){
             for(int j = i; j < n; j++){
-                swap(matrix,i,j);
+                swap1(matrix,i,j);
             }
         }
         
         for(int i = 0; i< n; i++){
             for(int j = 0; j < n/2; j++){
-                // swap(matrix,i,n-1-j);
-                int temp = 0;
-                temp = matrix[i][j];
-                matrix[i][j] = matrix[i][matrix.length - 1 - j];
-                matrix[i][matrix.length - 1 - j] = temp;
+                swap2(matrix,i,j);
+                
+                
             }
         }
         
@@ -21,9 +19,15 @@ class Solution {
         
     }
     
-    public void swap(int[][] arr, int i, int j){
+    public void swap1(int[][] arr, int i, int j){
         int temp = arr[i][j];
         arr[i][j] = arr[j][i];
         arr[j][i] = temp;
+    }
+    
+    public void swap2(int[][] arr, int i, int j){
+        int temp = arr[i][j];
+        arr[i][j] = arr[i][arr.length - 1 - j];
+        arr[i][arr.length - 1 - j] = temp;
     }
 }
