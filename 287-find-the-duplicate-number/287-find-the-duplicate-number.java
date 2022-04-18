c4c2e5961fb888a2249c65bf9915866403850f72
Aditya Chatterjee <48512350+adi1506@users.noreply.github.com>
2022-04-18 17:37:18 +0530

    Time: 39 ms (23.30%), Space: 60.2 MB (81.47%) - LeetHub

---
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        // //1st attempt : brute force : TC : O(N^2) ; SC : O(1)
+        // int n = nums.length-1;
+        // for(int i = 0 ; i<=n; i++){
+        //     for(int j = i+1; j <=n; j++){
+        //         if(nums[i] == nums[j])
+        //             return nums[i];
+        //     }
+        // }
+        // return 0;
+        
+        //2nd attempt : works only when the repeated int appears only twice
+        //(but in the question it is given that it can appear more than twice)
+        // int n = nums.length - 1;
+        // int sum = n*(n+1)/2;
+        // int arrSum = 0;
+        // for(int num : nums) arrSum+=num;
+        // // System.out.println(arrSum+" "+sum);
+        // return arrSum-sum;
+        
+        //3rd attempt: sort the array and if arr[i] == arr[i+1] then arr[i] is duplicate
+        Arrays.sort(nums);
+        for(int i = 0; i<nums.length-1; i++){
+            if(nums[i] == nums[i+1])
+                return nums[i];
+        }
+        return 0;
+    }
+}
