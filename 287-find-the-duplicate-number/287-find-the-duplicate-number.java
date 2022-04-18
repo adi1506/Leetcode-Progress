b409bd09da06b2f21ca84fa682df57ddf3a3aa3c
Aditya Chatterjee <48512350+adi1506@users.noreply.github.com>
2022-04-18 20:00:15 +0530

    Time: 4 ms (92.50%), Space: 60.2 MB (81.47%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.java
@@ -21,11 +21,33 @@ class Solution {
         
         //3rd attempt: sort the array and if arr[i] == arr[i+1] then arr[i] is duplicate
         //TC: O(nlogn + n)
-        Arrays.sort(nums);
-        for(int i = 0; i<nums.length-1; i++){
-            if(nums[i] == nums[i+1])
-                return nums[i];
+        //SC: O(logn) // Array.sort uses this much space
+        // Arrays.sort(nums);
+        // for(int i = 0; i<nums.length-1; i++){
+        //     if(nums[i] == nums[i+1])
+        //         return nums[i];
+        // }
+        // return 0;
+        
+        //4th attempt: Linked List Cycle and Tortoise-Hare Algo
+        
+        int slow = nums[0];
+        int fast = nums[0];
+        
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow != fast);
+        
+        fast = nums[0];
+        
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        return 0;
+        
+        return slow;
+        
+        
     }
 }
