50cd7652df48d09b313f8e0eca76fe7c34103e7a
Aditya Chatterjee <48512350+adi1506@users.noreply.github.com>
2022-04-19 13:18:42 +0530

    Time: 1 ms (33.23%), Space: 42.9 MB (35.27%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -1,16 +1,47 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         //1st attempt : brute force
+        //TC : O(n^2)
+        //SC : O(1);
+//         int m = matrix.length;
+//         int n = matrix[0].length;
+        
+//         for(int i = 0 ; i < m ; i++){
+//             for(int j = 0 ; j < n ; j++){
+//                 if(matrix[i][j] == target)
+//                     return true;
+//             }
+//         }
+        
+//         return false;
+        
+        //2nd attempt : 
         int m = matrix.length;
         int n = matrix[0].length;
         
-        for(int i = 0 ; i < m ; i++){
-            for(int j = 0 ; j < n ; j++){
-                if(matrix[i][j] == target)
-                    return true;
-            }
+        int row = 0;
+        for(int i = 0; i<m; i++){
+            if(target >= matrix[i][0] && target <= matrix[i][n-1])
+                row = i;
         }
         
-        return false;
+        int start = 0;
+        int end = n-1;
+        
+        
+        while(start <= end){
+            int mid = (start+end)/2;
+            if(target == matrix[row][mid])
+                return true;
+            else if(target < matrix[row][mid])
+                end = mid-1;
+            else
+                start = mid+1;
+        }
+        if(start < n && matrix[row][start] == target)
+            return true;
+        else return false;
+        // System.out.println(row);
+        // return false;
     }
 }
