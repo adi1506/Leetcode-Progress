185bdb281e5e4bbcb3f59a63324633913e746bd5
Aditya Chatterjee <48512350+adi1506@users.noreply.github.com>
2022-04-19 20:18:14 +0530

    Time: 2 ms (82.32%), Space: 56.3 MB (43.31%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,22 +1,34 @@
 class Solution {
     public int majorityElement(int[] nums) {
         //1st attempt : brute force : TC : O(n^2) ; SC : O(1)
-        int n = nums.length;
-        int prev = Integer.MIN_VALUE;
-        int count = 0;
-        for(int i = 0; i < n; i++){
-            if(nums[i] != prev){
-                for(int j = 0; j< n; j++){
-                    if(nums[j] == nums[i])
-                        count++;
-                }    
-            }
-            prev = nums[i];
-            if(count > n/2)
-                return nums[i];
-            else
-                count = 0;
+        // int n = nums.length;
+        // int prev = Integer.MIN_VALUE;
+        // int count = 0;
+        // for(int i = 0; i < n; i++){
+        //     if(nums[i] != prev){
+        //         for(int j = 0; j< n; j++){
+        //             if(nums[j] == nums[i])
+        //                 count++;
+        //         }    
+        //     }
+        //     prev = nums[i];
+        //     if(count > n/2)
+        //         return nums[i];
+        //     else
+        //         count = 0;
+        // }
+        // return 0;
+        
+        //2nd attempt : Moore's Voting Algorithm
+        
+        int cnt = 0;
+        int el = 0;
+        
+        for(int i = 0; i < nums.length; i++){
+            if(cnt == 0) el = nums[i];
+            if(el == nums[i]) cnt++;
+            else cnt--;
         }
-        return 0;
+        return el;
     }
 }
