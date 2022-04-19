2497a7171477e3231675c1e4f91a4f5683356e2d
Aditya Chatterjee <48512350+adi1506@users.noreply.github.com>
2022-04-19 19:00:59 +0530

    Time: 1 ms (78.38%), Space: 43.2 MB (38.52%) - LeetHub

--- 50-powx-n/50-powx-n.java
+++ 50-powx-n/50-powx-n.java
@@ -12,7 +12,28 @@ class Solution {
         
 //         return res;
         
-        //2nd attempt 
-        return Math.pow(x,n);
+        //2nd attempt : used inbuilt library XD
+        // return Math.pow(x,n);
+        
+        //3rd attempt : iterative/mathematical approach
+        
+        double ans = 1.0;
+        long nn = n;
+        if(nn < 0) nn *= -1;
+        
+        while(nn > 0){
+            if(nn % 2 == 1){
+                ans = ans * x;
+                nn = nn -1;
+            }
+            else{
+                x = x*x;
+                nn = nn / 2;
+            }
+        }
+        if(n < 0) ans = (double)1.0/(double)(ans); 
+        return ans;
+        
+        
     }
 }
