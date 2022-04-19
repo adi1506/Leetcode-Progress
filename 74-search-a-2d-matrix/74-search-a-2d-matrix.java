b8fac465ed1163195e5e557d95f12c3b6754f8a8
Aditya Chatterjee <48512350+adi1506@users.noreply.github.com>
2022-04-19 13:36:01 +0530

    Time: 1 ms (33.23%), Space: 43.4 MB (11.60%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -15,33 +15,54 @@ class Solution {
         
 //         return false;
         
-        //2nd attempt : 
+        //2nd attempt : finding the row in which the target is and performing binary search on that row to find the target
+        //TC: O(n + logn) //n for searching row and logn for Binary Search
+//         int m = matrix.length;
+//         int n = matrix[0].length;
+        
+//         int row = 0;
+//         for(int i = 0; i<m; i++){
+//             if(target >= matrix[i][0] && target <= matrix[i][n-1])
+//                 row = i;
+//         }
+        
+//         int start = 0;
+//         int end = n-1;
+        
+        
+//         while(start <= end){
+//             int mid = (start+end)/2;
+//             if(target == matrix[row][mid])
+//                 return true;
+//             else if(target < matrix[row][mid])
+//                 end = mid-1;
+//             else
+//                 start = mid+1;
+//         }
+//         // if(start < n && matrix[row][start] == target)
+//         //     return true;
+//         // else return false;
+//         // System.out.println(row);
+//         return false;
+        
+        //3rd attempt : performing binary search directly on 2D matrix
+        
         int m = matrix.length;
         int n = matrix[0].length;
         
-        int row = 0;
-        for(int i = 0; i<m; i++){
-            if(target >= matrix[i][0] && target <= matrix[i][n-1])
-                row = i;
-        }
+        int low = 0;
+        int high = m*n -1;
         
-        int start = 0;
-        int end = n-1;
-        
-        
-        while(start <= end){
-            int mid = (start+end)/2;
-            if(target == matrix[row][mid])
+        while(low <= high){
+            int mid = (low + high)/2;
+            if(target == matrix[mid/n][mid%n])
                 return true;
-            else if(target < matrix[row][mid])
-                end = mid-1;
+            else if(target < matrix[mid/n][mid%n])
+                high = mid - 1;
             else
-                start = mid+1;
+                low = mid + 1;
         }
-        // if(start < n && matrix[row][start] == target)
-        //     return true;
-        // else return false;
-        // System.out.println(row);
+        
         return false;
     }
 }
