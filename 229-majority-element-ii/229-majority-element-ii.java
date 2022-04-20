de10eafa563bddb66c6f51ee3ed6ec7259944457
Aditya Chatterjee <48512350+adi1506@users.noreply.github.com>
2022-04-20 07:48:56 +0530

    Time: 2 ms (90.99%), Space: 49.5 MB (61.02%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.java
+++ 229-majority-element-ii/229-majority-element-ii.java
@@ -1,24 +1,70 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int n = nums.length;
-        int count = 0;
-        List<Integer> maj = new ArrayList<Integer>();
-        for(int i = 0;i < n; i++){
+        //1st attempt : brute force
+        // TC : O(N^2)
+        // SC : O(1)
+//         int n = nums.length;
+//         int count = 0;
+//         List<Integer> maj = new ArrayList<Integer>();
+//         for(int i = 0;i < n; i++){
             
-            for(int j = i; j< n; j++){
-                if(nums[i] == nums[j]){
-                    count++;
-                }
-            }
+//             for(int j = i; j< n; j++){
+//                 if(nums[i] == nums[j]){
+//                     count++;
+//                 }
+//             }
             
           
-            if(count > n/3 && !maj.contains(nums[i]))
-                maj.add(nums[i]);
+//             if(count > n/3 && !maj.contains(nums[i]))
+//                 maj.add(nums[i]);
             
-            count = 0;
+//             count = 0;
                 
+//         }
+        
+//         return maj;
+        
+        //2nd attempt : Moore's Voting Algorithm
+        List<Integer> maj = new ArrayList<Integer>();
+        int n = nums.length;
+        
+        int el1 = -1;
+        int el2 = -1;
+        int count1 = 0;
+        int count2 = 0;
+        
+        for(int num : nums){
+            if(el1 == num){
+                count1++;
+            }
+            else if(el2 == num){
+                count2++;
+            }
+            else if(count1 == 0){
+                el1 = num;
+                count1 = 1;
+            }
+            else if(count2 == 0){
+                el2 = num;
+                count2 = 1;
+            }
+            else{
+                count1--;
+                count2--;
+            }
         }
         
+        int countEl1 = 0;
+        int countEl2 = 0;
+        for(int num : nums){
+            if(num == el1) countEl1++;
+            else if(num == el2) countEl2++;
+        }
+        
+        if(countEl1 > n/3) maj.add(el1);
+        if(countEl2 > n/3) maj.add(el2);
+        
         return maj;
+        
     }
 }
