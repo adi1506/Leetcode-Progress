69ef060ce9e4948fa17307b4be4f20610175fc8c
Aditya Chatterjee <48512350+adi1506@users.noreply.github.com>
2022-04-25 19:54:04 +0530

    Time: 13 ms (42.95%), Space: 45.1 MB (41.60%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -22,78 +22,52 @@ class Solution {
         
 //         return ls;
         
-        //2nd attempt
-        
+        //2nd attempt : 
+        //TC : O(2N) : the string is traversed 2 times : once by r and l
+        //SC : O(N) : HashSet used
+   
 //         int n = s.length();
         
-//         if(n == 0 || n == 1)
-//             return s.length();
+//         if(n <= 1)
+//             return n;
         
+//         int maxans = 0;
 //         int l = 0;
-//         int r = 0;
-        
-//         int ls = 0;
 //         Set<Character> hs = new HashSet<Character>();
         
-//         while((l < r) || (r > n)){
+//         for(int r = 0; r < n ; r++){
 //             if(hs.contains(s.charAt(r))){
-//                 hs.remove(s.charAt(l));
-//                 l++;
-//             }
-//             else{
-                
-//                 hs.add(s.charAt(r));
-//                 r++;
-//             }
-//             ls = Math.max(ls,r-l+1);
-//         }
-        
-//         return ls;
-        
-        //3rd attempt
-        
-//         int n = s.length();
-        
-//         if(n == 0 || n == 1)
-//             return s.length();
-        
-        
-//         int maxans = Integer.MIN_VALUE;
-//         Set < Character > set = new HashSet < > ();
-//         int l = 0;
-//         for (int r = 0; r < n; r++) // outer loop for traversing the string
-//         {
-//             if (set.contains(s.charAt(r))) //if duplicate element is found
-//             {
-//                 while (l < r && set.contains(s.charAt(r))) {
-//                     set.remove(s.charAt(l));
+//                 while(l < r && hs.contains(s.charAt(r))){
+//                     hs.remove(s.charAt(l));
 //                     l++;
 //                 }
 //             }
-//             set.add(s.charAt(r));
-//             maxans = Math.max(maxans, r - l + 1);
+//             hs.add(s.charAt(r));
+//             maxans = Math.max(maxans,r-l+1);
 //         }
+        
 //         return maxans;
         
+        //3rd attempt : 
         
         int n = s.length();
         
-        if(n <= 1)
+        if(n<=1)
             return n;
         
-        int maxans = 0;
-        int l = 0;
-        Set<Character> hs = new HashSet<Character>();
+        HashMap<Character, Integer> hm = new HashMap<Character, Integer>();
         
-        for(int r = 0; r < n ; r++){
-            if(hs.contains(s.charAt(r))){
-                while(l < r && hs.contains(s.charAt(r))){
-                    hs.remove(s.charAt(l));
-                    l++;
-                }
+        int l = 0;
+        int maxans = 0;
+        
+        for(int r = 0; r < n; r++){
+            if(hm.containsKey(s.charAt(r))){
+                l = Math.max(l,hm.get(s.charAt(r)) +1);
+                // hm.put(s.charAt(r),r);
             }
-            hs.add(s.charAt(r));
-            maxans = Math.max(maxans,r-l+1);
+            hm.put(s.charAt(r),r);
+            maxans = Math.max(maxans, r-l+1);
+            
         }
         
         return maxans;
