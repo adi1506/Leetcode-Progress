1fb07ec81078e1e7d0aaa1999ed54af44c1540b1
Aditya Chatterjee <48512350+adi1506@users.noreply.github.com>
2024-05-23 11:40:25 +0530

    Time: 16 ms (39.06%) | Memory: 20.9 MB (20.18%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -39,13 +39,43 @@ public:
         // return n*(n+1)/2 - sumOfArr;
 
         //method 4 : arithmetic XOR
-        int res1 = 0;
-        int res2 = 0;
-        for(int i = 0; i<n; i++){
-            res1 ^= (i+1);
-            res2 ^= arr[i];
+        // int res1 = 0;
+        // int res2 = 0;
+        // for(int i = 0; i<n; i++){
+        //     res1 ^= (i+1);
+        //     res2 ^= arr[i];
+        // }
+
+        // return res1^res2;
+
+        //method 5 : binary search
+
+        sort(arr.begin(), arr.end());
+
+        int s = 0;
+        int e = n-1;
+        int mid = s + (e-s)/2;
+        int ansIndex = -1;
+
+        while (s<=e){
+            int num = arr[mid];
+            int index = mid;
+            int diff = num-index;
+
+            if(diff == 0){
+                s = mid +1;
+            }
+            else if(diff == 1){
+                ansIndex = mid;
+                e = mid - 1;
+            }
+            mid = s + (e-s)/2;
         }
 
-        return res1^res2;
+        
+        if(ansIndex == -1){
+            return n;
+        }
+        return ansIndex;
     }
 };
