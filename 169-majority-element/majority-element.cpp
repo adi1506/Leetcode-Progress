78a8f1066a772118ddeebd7896b85920db8b7d87
Aditya Chatterjee <48512350+adi1506@users.noreply.github.com>
2024-05-27 20:35:21 +0530

    Time: 15 ms (77.70%) | Memory: 26.5 MB (99.06%) - LeetSync

---
+++ 169-majority-element/majority-element.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& arr) {
+        int n = arr.size();
+        return method3(arr, n);
+    }
+
+    //method 1 -> brute force -> finding the element which is occuring for the max times
+    int method1(vector<int>& arr, int n){
+        for(int i =0; i<n;i++){
+            int tempCount = 0;
+            for(int j =i; j<n; j++){
+                if(arr[i] == arr[j]){
+                    tempCount++;
+                }
+            }
+            if(tempCount > (n/2)){
+                return arr[i];
+            }
+        }
+        return 0;
+    }
+
+    //method 2 -> Hashing
+    int method2(vector<int>& arr, int n){
+        //declare a map -> <el, freq>
+        map<int, int> mpp;
+
+        //frequency map
+        for(int i = 0; i<n; i++){
+            mpp[arr[i]]++;
+        }
+
+        //find the element whose count is > n/2
+        for(auto it: mpp){
+            if(it.second > n/2){
+                return it.first;
+            }
+        }
+        return -1;
+    }
+
+    int method3(vector<int>& arr, int n){
+        int el;
+        int count = 0;
+        for(int i = 0; i< n; i++){
+            if(count == 0){
+                count = 1;
+                el = arr[i];
+            }
+            else if(arr[i] == el){
+                count++;
+            }
+            else{
+                count--;
+            }
+            
+        }
+        return el;
+    }
+};
