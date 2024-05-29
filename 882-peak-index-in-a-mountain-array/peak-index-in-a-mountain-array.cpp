21cad81336524fd977fcb22d63e74af4876e23a2
Aditya Chatterjee <48512350+adi1506@users.noreply.github.com>
2024-05-29 22:04:25 +0530

    Time: 78 ms (74.14%) | Memory: 62.1 MB (43.95%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-        return method2(arr);
+        return method3(arr);
     }
 
     //method1 - linear search 
@@ -35,21 +35,23 @@ public:
         return mid;
     }
 
+    //store and compute
     int method3(vector<int> &arr){
         int n = arr.size();
         int s = 0;
         int e = n-1;
         int mid = s+ (e-s)/2;
-
-        while(s<e){
+        int ansIndex = -1;
+        while(s<=e){
             if(arr[mid] < arr[mid+1]){
                 s = mid +1;
             }
             else{
-                e = mid;
+                ansIndex = mid;
+                e = mid-1;
             }
             mid = s + (e-s)/2;
         }
-        return mid;
+        return ansIndex;
     }
 };
