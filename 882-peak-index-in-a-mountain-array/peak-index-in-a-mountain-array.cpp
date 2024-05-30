aca2c294caada5e082b8c031805860269654f1e8
Aditya Chatterjee <48512350+adi1506@users.noreply.github.com>
2024-05-30 17:11:31 +0530

    Time: 88 ms (22.68%) | Memory: 62 MB (81.67%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -1,57 +1,22 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-        return method3(arr);
-    }
-
-    //method1 - linear search 
-    int method1(vector<int> &arr){
-        int max = INT_MIN;
-        int maxIndex = -1;
-        for(int i = 0; i<arr.size();i++){
-            if(arr[i] > max){
-                max = arr[i];
-                maxIndex = i;
-            }
-        }
-        return maxIndex;
-    }
-
-    int method2(vector<int> &arr){
         int n = arr.size();
-        int s = 0;
+        int s = 0; 
         int e = n-1;
-        int mid = s+ (e-s)/2;
+        int mid = s + (e-s)/2;
+        int peakIndex = 0;
 
-        while(s<e){
-            if(arr[mid] < arr[mid+1]){
-                s = mid +1;
-            }
-            else{
-                e = mid;
-            }
-            mid = s + (e-s)/2;
-        }
-        return mid;
-    }
-
-    //store and compute
-    int method3(vector<int> &arr){
-        int n = arr.size();
-        int s = 0;
-        int e = n-1;
-        int mid = s+ (e-s)/2;
-        int ansIndex = -1;
         while(s<=e){
             if(arr[mid] < arr[mid+1]){
                 s = mid +1;
             }
             else{
-                ansIndex = mid;
-                e = mid-1;
+                peakIndex = mid;
+                e = mid - 1;
             }
-            mid = s + (e-s)/2;
+            mid = s+ (e-s)/2;
         }
-        return ansIndex;
+        return peakIndex;
     }
 };
