2ded6f401037f085ed5db30a2204999d1855bab4
Aditya Chatterjee <48512350+adi1506@users.noreply.github.com>
2024-05-30 20:17:46 +0530

    Time: 8 ms (9.75%) | Memory: 13.6 MB (35.51%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -4,36 +4,35 @@ public:
     int findPivot(vector<int>& arr, int n){
         int s = 0;
         int e = n-1;
-        int mid = s+(e-s)/2;
+        int mid = s + (e-s)/2;
+        int pivotIndex = -1;
 
         while(s<=e){
             if(s==e){
                 return s;
             }
-            if((mid+1 < n) && (arr[mid] > arr[mid+1])){
+            else if(mid+1 < n && arr[mid] > arr[mid+1]){
                 return mid;
             }
-            if(arr[mid] < arr[s]){
+            else if(arr[mid] < arr[s]){
                 e = mid -1;
             }
             else{
-                s = mid+1;
+                s = mid + 1;
             }
             mid = s+(e-s)/2;
         }
         return -1;
-
     }
 
-    int binarySearch(vector<int>& arr, int n, int s, int e, int target){
-        int mid = s+(e-s)/2;
-
+    int binarySearch(vector<int> &arr, int n, int s, int e, int target){
+        int mid = s + (e-s)/2;
         while(s<=e){
             if(arr[mid] == target){
                 return mid;
             }
-            else if(arr[mid] < target){
-                s = mid+1;
+            else if(arr[mid] <= target){
+                s = mid +1;
             }
             else{
                 e = mid -1;
@@ -41,26 +40,21 @@ public:
             mid = s+(e-s)/2;
         }
         return -1;
-
     }
 
+    
     int search(vector<int>& arr, int target) {
         int n = arr.size();
         int pivot = findPivot(arr, n);
 
-        if(target >= arr[0] && target<= arr[pivot]){
-            int ans =  binarySearch(arr, n, 0 , pivot, target);
+        if(target >= arr[0] && target <= arr[pivot]){
+            int ans = binarySearch(arr, n, 0, pivot, target);
             return ans;
         }
         else{
-            int ans = binarySearch(arr, n, pivot+1 , n-1, target);
+            int ans = binarySearch(arr, n, pivot+1, n-1, target);
             return ans;
         }
-
         return -1;
-
     }
-
-    
 };
-
