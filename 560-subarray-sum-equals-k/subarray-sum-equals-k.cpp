523832bc3bed9ed6d76487f3c8104bd5367167f7
Aditya Chatterjee <48512350+adi1506@users.noreply.github.com>
2024-06-02 06:55:28 +0530

    Time: 101 ms (35.52%) | Memory: 52.5 MB (7.99%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         int n = nums.size();
-        return method1(nums, k, n);
+        return method2(nums, k, n);
     }
 
     //method1
@@ -18,5 +18,23 @@ public:
             }
         }
         return sumCount;
+    }
+
+    //method2
+    int method2(vector<int> &arr, int k, int n){
+        map<int, int> mpp;
+        mpp[0] = 1;
+        int presum = 0;
+        int count = 0;
+
+        for(int i = 0; i< n; i++){
+            presum += arr[i];
+            count += mpp[presum-k];
+            mpp[presum]++;
+        }
+
+    
+
+        return count;
     }
 };
