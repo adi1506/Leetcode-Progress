d34a10fafd7763d9c9b6421d79276177a142f1a9
Aditya Chatterjee <48512350+adi1506@users.noreply.github.com>
2024-06-05 21:12:38 +0530

    Time: 6 ms (24.11%) | Memory: 9.4 MB (41.09%) - LeetSync

---
+++ 2406-decode-the-message/decode-the-message.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string decodeMessage(string key, string message) {
+        unordered_map<char, char> mapping;
+        char space = ' ';
+        mapping[space] = space;
+        char start = 'a';
+        int index = 0;
+
+        //mapping
+        while(start<= 'z' && index < key.length()){
+            if(mapping.find(key[index]) != mapping.end()){
+                index++;
+            }
+            else{
+                mapping[key[index]] = start; 
+                index++;
+                start++;
+            }
+        }
+        
+
+        //decoding
+        string ans = "";
+        for(int i = 0; i<message.length(); i++){
+            char msgChar = message[i];
+            char mappedChar = mapping[msgChar];
+            ans.push_back(mappedChar);
+        }
+        return ans;
+    }
+};
