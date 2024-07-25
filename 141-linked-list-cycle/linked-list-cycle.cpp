b57b9578993250fdfa5b1c09f38a2c302696fa0c
Aditya Chatterjee <48512350+adi1506@users.noreply.github.com>
2024-07-25 12:53:46 +0530

    Time: 8 ms (61.85%) | Memory: 10.8 MB (28.30%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -9,6 +9,7 @@
 class Solution {
 public:
 
+    //method1 - using map
     bool method1(ListNode* &head){
         unordered_map<ListNode*, bool> mapping;
         ListNode* temp = head;
@@ -25,7 +26,26 @@ public:
         return false;
     }
 
+    //method2 - using floyyd tortoise algo
+    bool method2(ListNode* &head){
+        ListNode* fast = head;
+        ListNode* slow = head;
+
+        while(fast != NULL){
+            fast = fast->next;
+            if(fast!= NULL){
+                fast = fast -> next;
+                slow = slow -> next;
+            }
+            if(fast == slow){
+                return true;
+            }
+        }
+        return false;
+    }
+
     bool hasCycle(ListNode *head) {
-        return method1(head);
+        // return method1(head);
+        return method2(head);
     }
 };
